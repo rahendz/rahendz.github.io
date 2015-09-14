e9d863d70ce99e2548831042404013ab71b46b05
Rahendra Putra K <rahendz@gmail.com>
2015-09-14 15:19:35 +0700

    change menu into material slide design

--- js/trigger.js
+++ js/trigger.js
@@ -6,4 +6,17 @@ $(document).ready(function(){
 		// $(this).parent().addClass('active');
 		$('html').animate({scrollTop: target.offset().top},'slow');
 	});
+	$('[role="button-toggle"]').on('click',function(){
+		var target = $(this).data('target');
+		$(this).toggleClass('toggled in');
+		$(target).toggleClass('in');
+		$('.aside-overlay').toggleClass('in');
+	});
+	$('.aside-overlay').on('click',function(){
+		var btnToggle = $('[role=button-toggle]'),
+			target = btnToggle.data('target');
+		$(this).toggleClass('in');
+		btnToggle.toggleClass('in');
+		$(target).toggleClass('in');
+	});
 });
